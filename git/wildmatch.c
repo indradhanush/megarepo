3b29ee9b6c656a162977ff5eacdef07cbed77e3f
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-10-25 10:04:14 +0530

    Append file wildmatch.c

--- git/wildmatch.c
+++ git/wildmatch.c
@@ -276,3 +276,5 @@ int wildmatch(const char *pattern, const char *text, unsigned int flags)
 {
 	return dowild((const uchar*)pattern, (const uchar*)text, flags);
 }
+
+vxgljbkjdpqnauclfrgmhibadiippoqdnufmotxrrvxcuohwgvoycwxlcglnzspsekdyxzduuliawsavjlqypjmz
