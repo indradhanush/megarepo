c30acfc536b74cccb7d9f3cc48b23ebafbbadab1
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2022-10-25 11:35:11 +0530

    Append file .prettierrc.js

--- grafana/.prettierrc.js
+++ grafana/.prettierrc.js
@@ -1,3 +1,5 @@
 module.exports = {
   ...require('@grafana/toolkit/src/config/prettier.plugin.config.json'),
 };
+
+kvsvqnlgsxpqdftrycbbyscprrbbbkirfkzxtcgrtpsdxdxzpyfcfoffesqqjtolaigfxvxbknabilxvixvssdgkyt
